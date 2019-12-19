025eb6641a1dbb0bb277fa67afd420e302783999
kodurivamshi <vkoduri555@gmail.com>
2019-12-19 23:44:56 +0530

    login page

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ app.use(express.static('public'))
 //app.get('/', (req, res) => res.sendFile('Hello world...'))
 app.get('/', (req, res) => res.sendFile(__dirname+'/public/htmls/index1.html'))
 
+app.get("/index", (req, res) => res.sendFile(__dirname+'/public/htmls/index.html'))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
