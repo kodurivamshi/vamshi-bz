8585252a521320def6d13b0d4bdf5ce35dd79c2d
kodurivamshi <vkoduri555@gmail.com>
2019-12-20 11:16:56 +0530

    data from express retrieved

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.use(express.static('public'))
 //app.get('/', (req, res) => res.sendFile('Hello world...'))
 app.get('/', (req, res) => res.sendFile(__dirname+'/public/htmls/index1.html'))
 
+app.get("/index2", (req, res) => res.sendFile(__dirname+'/public/htmls/index2.html'))
 app.get("/index", (req, res) => res.sendFile(__dirname+'/public/htmls/index.html'))
+
+var obj={}
+obj.name = "Being Zero";
+obj.college = "CMRCET";
+obj.regno = "111111ABC";
+
+app.get("/data", (req, res) => res.json(obj))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ public/js/javascript1.js
@@ -0,0 +1,14 @@
+// page fully loaded
+//  1. get data from server - url ? /data
+//  2. show data in html 
+
+$(document).ready(function(){
+    $("#bt").click(function(){
+	$.getJSON("/data", function(d){
+		alert(JSON.stringify(d));
+		$("#name").text(d.name);
+		$("#college").text(d.college);
+		$("#regno").text(d.regno);
+	})
+    })
+})
